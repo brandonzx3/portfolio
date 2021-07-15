aba6171f837652d6b608e2894305b13b6653f7fe
Brandonzx3 <btherr2003@gmail.com>
2021-07-15 00:59:57 -0500

    e

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { env } = require('process');
 const app = express();
-const PORT = 3000 || env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('client'));
 
