9070c1221216be72885d55e630a4c91e383985d5
Brandonzx3 <btherr2003@gmail.com>
2021-07-15 00:56:29 -0500

    hurting

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { env } = require('process');
 const app = express();
-const port = 3000 || env.PORT;
+const PORT = 3000 || env.PORT;
 
 app.use(express.static('client'));
 
 app.listen(port, () => {
-    console.log(`app running on port ${port}`);
+    console.log(`app running on port ${PORT}`);
 })
