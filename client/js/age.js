b61e8ac943dfa13902fad25b8c9d6597b44d8419
brandon <btherr2003@gmail.com>
2021-11-18 22:28:59 -0600

    made things work

---
+++ client/js/age.js
@@ -0,0 +1,14 @@
+let ageEL = document.getElementById("age");
+
+
+var age, daysBetweenDates;
+daysBetweenDates = function(d1, d2) {
+  var diffDays, oneDay;
+  oneDay = 24 * 60 * 60 * 1000;
+  diffDays = (d2 - Date.parse(d1)) / oneDay;
+  return diffDays;
+};
+
+setInterval(() => {
+    ageEL.innerHTML = (daysBetweenDates('Jun 12, 2003 14:48:00', new Date()) / 365).toString().substring(0, 12);
+});
