642b66e69509f18873edda2ad6dc9dee03e1032e
brandon <btherr2003@gmail.com>
2022-01-08 15:47:15 -0600

    made a theme switcher

---
+++ client/js/theme.js
@@ -0,0 +1,29 @@
+var buttonEl = document.querySelector("#themeButton");
+var root = document.querySelector(":root");
+
+console.log(localStorage.getItem("theme"));
+if(localStorage.getItem("theme") == null) {
+    localStorage.setItem("theme", "dark");
+}
+
+buttonEl.onclick = function() {
+    document.querySelector("#switch").play();
+
+    if(localStorage.getItem("theme") == "dark") {
+        localStorage.setItem("theme", "light");
+        root.style.setProperty("--background", "white")
+        root.style.setProperty("--foreground", "black")
+    } else {
+        localStorage.setItem("theme", "dark");
+        root.style.setProperty("--background", "black")
+        root.style.setProperty("--foreground", "white")
+    }
+}
+
+if(localStorage.getItem("theme") == "dark") {
+    root.style.setProperty("--background", "black")
+    root.style.setProperty("--foreground", "white")
+} else {
+    root.style.setProperty("--background", "white")
+    root.style.setProperty("--foreground", "black")
+}
