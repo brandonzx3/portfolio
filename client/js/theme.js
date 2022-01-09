46ee303f063e89e3d3bf4cf1f5a99e9309add911
brandon <btherr2003@gmail.com>
2022-01-08 18:38:50 -0600

    added a lightbulb icon

--- client/js/theme.js
+++ client/js/theme.js
@@ -7,7 +7,6 @@ if(localStorage.getItem("theme") == null) {
 }
 var switchEffect = new Audio()
 buttonEl.onclick = function() {
-    //switchEffect.pause();
     switchEffect = new Audio("./assets/switch.mp3");
     switchEffect.play();
 
@@ -15,17 +14,21 @@ buttonEl.onclick = function() {
         localStorage.setItem("theme", "light");
         root.style.setProperty("--background", "white")
         root.style.setProperty("--foreground", "black")
+        buttonEl.style.setProperty("color", "black");
     } else {
         localStorage.setItem("theme", "dark");
         root.style.setProperty("--background", "black")
         root.style.setProperty("--foreground", "white")
+        buttonEl.style.setProperty("color", "white");
     }
 }
 
 if(localStorage.getItem("theme") == "dark") {
     root.style.setProperty("--background", "black")
     root.style.setProperty("--foreground", "white")
+    buttonEl.style.setProperty("color", "white");
 } else {
     root.style.setProperty("--background", "white")
     root.style.setProperty("--foreground", "black")
+    buttonEl.style.setProperty("color", "black");
 }
