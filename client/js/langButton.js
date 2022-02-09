a40cc4fc9333c001743ab242e6e3df0c71c6cb8a
Coding Rooms <git@codingrooms.io>
2022-02-09 14:54:01 +0000

    changed some stuff

--- client/js/langButton.js
+++ client/js/langButton.js
@@ -2,6 +2,21 @@ const buttons = document.getElementsByClassName("lang_button")
 
 Array.from(buttons).forEach(button => {
     button.addEventListener("click", () => {
-        alert("clicked a lang button");
+        redirect(button.innrtText);
     });
 });
+
+const javaRepos = [];
+const nodeRepos = ["https://github.com/brandonzx3/portfolio"];
+const lvRepos = [];
+
+function redirect(name) {
+    let links = [];
+    switch(name) {
+        case "Java": links = javaRepos; break;
+        case "NodeJS": links = nodeRepos; break;
+        case "Labview": links = lvRepos; break;
+        default: console.log("something went so wrong");
+    }
+    window.open(links[Math.floor(Math.random()*links.length)], "_blank");
+}
