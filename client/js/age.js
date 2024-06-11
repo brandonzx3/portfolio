00602081239d393a5db7374b76ad529b9298092e
Brandonzx3 <btherr2003@gmail.com>
2024-06-11 12:02:43 -0500

    fixed leap year in age calculation

--- client/js/age.js
+++ client/js/age.js
@@ -1,13 +1,23 @@
 let ageEL = document.getElementById("age");
 
-var age, daysBetweenDates;
-daysBetweenDates = function(d1, d2) {
-  var diffDays, oneDay;
-  oneDay = 24 * 60 * 60 * 1000;
-  diffDays = (d2 - Date.parse(d1)) / oneDay;
-  return diffDays;
-};
+function isLeapYear(year) {
+    var d = new Date(year, 1, 28);
+    d.setDate(d.getDate() + 1);
+    return d.getMonth() == 1;
+}
+
+function getAge(date) {
+    var d = new Date(date), now = new Date();
+    var years = now.getFullYear() - d.getFullYear();
+    d.setFullYear(d.getFullYear() + years);
+    if (d > now) {
+        years--;
+        d.setFullYear(d.getFullYear() - 1);
+    }
+    var days = (now.getTime() - d.getTime()) / (3600 * 24 * 1000);
+    return years + days / (isLeapYear(now.getFullYear()) ? 366 : 365);
+}
 
 setInterval(() => {
-    ageEL.innerHTML = (daysBetweenDates('Jun 15, 2003 14:48:00', new Date()) / 365).toString().substring(0, 12);
+    ageEL.innerHTML = (getAge('Jun 12, 2003')).toString().substring(0, 12);
 });
